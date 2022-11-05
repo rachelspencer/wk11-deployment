8b425960da830307ce83cae84cd7e750c48c18b4
Rachel Spencer <rachelspencer688@gmail.com>
2022-11-05 11:15:29 -0400

    Ready for deployment

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+const port = process.env.PORT || 4005
+
+app.listen(port, () => {
+    console.log('Port running on 4005.')
+})
